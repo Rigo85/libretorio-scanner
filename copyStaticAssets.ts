dd268e242a24c723974ca026bf7d5e9fd91468c3
Rigoberto Leander Salgado Reyes <rlsalgado2006@gmail.com>
2024-08-23 11:49:31 -0500

    updating copyStaticAssets to work on pm2

--- copyStaticAssets.ts
+++ copyStaticAssets.ts
@@ -2,13 +2,19 @@ import shell from "shelljs";
 import path from "path";
 
 shell.cp("-R", "src/services/calibre", "dist/services");
-shell.mkdir("-p", "dist/public/covers");
-shell.mkdir("-p", "dist/public/temp_covers");
-shell.mkdir("-p", "dist/public/cache");
-shell.mkdir("-p", "dist/public/books");
+// shell.mkdir("-p", "dist/public/covers");
+// shell.mkdir("-p", "dist/public/temp_covers");
+// shell.mkdir("-p", "dist/public/cache");
+// shell.mkdir("-p", "dist/public/books");
+shell.mkdir("-p", "dist/public");
 
-const publicDir = path.join("dist/public", __dirname.slice(1), "dist/public");
-shell.mkdir("-p", publicDir);
-shell.ln("-s", path.join(__dirname, "dist/public/books"), path.join(publicDir, "books"));
+// const publicDir = path.join("dist/public", __dirname.slice(1), "dist/public");
+// shell.mkdir("-p", publicDir);
+// shell.ln("-s", path.join(__dirname, "dist/public/books"), path.join(publicDir, "books"));
+shell.ln("-s", "/media/RIGO7/BACKUP/LIBROS", "dist/public/books");
+shell.ln("-s", "/media/RIGO7/Libretorio-conf/covers", "dist/public/covers");
+shell.ln("-s", "/media/RIGO7/Libretorio-conf/cache", "dist/public/cache");
+shell.ln("-s", "/media/RIGO7/Libretorio-conf/temp_covers", "dist/public/temp_covers");
 
-shell.cp("-R", "src/browser/*", "dist/public");
+
+// shell.cp("-R", "src/browser/*", "dist/public");
